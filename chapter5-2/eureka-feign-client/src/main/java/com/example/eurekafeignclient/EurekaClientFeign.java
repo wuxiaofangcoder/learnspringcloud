7dda728613e5360231493c0794e5a5945392724d
wxf-xiaozhi <983053245@qq.com>
2018-12-06 19:17:26 +0800

    second add

--- chapter5-2/eureka-feign-client/src/main/java/com/example/eurekafeignclient/EurekaClientFeign.java
+++ chapter5-2/eureka-feign-client/src/main/java/com/example/eurekafeignclient/EurekaClientFeign.java
@@ -1,7 +1,11 @@
 package com.example.eurekafeignclient;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "eureka-client",configuration = Fe)
+@FeignClient(value = "eureka-client",configuration = FeignConfig.class)
 public interface EurekaClientFeign {
+    @GetMapping(value = "/hi")
+    String sayHiFromClientEureka(@RequestParam(value = "name") String name);
 }
--- chapter5-2/eureka-feign-client/src/main/java/com/example/eurekafeignclient/FeignConfig.java
+++ chapter5-2/eureka-feign-client/src/main/java/com/example/eurekafeignclient/FeignConfig.java
@@ -4,10 +4,14 @@ import feign.Retryer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+
 @Configuration
 public class FeignConfig {
     @Bean
     public Retryer feignRetryer(){
-        return new Retryer.Default(100,SECONDS.toMIllis(1),5);
+        return new Retryer.Default(100,
+                SECONDS.toMillis(1L),5);
     }
 }
