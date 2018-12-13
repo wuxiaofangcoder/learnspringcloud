1a5a9f6cae07cd4abdf368d3c42f5a76a5dfe0fc
wxf-xiaozhi <983053245@qq.com>
2018-12-13 20:50:29 +0800

    20181213代码提交，微服务链路追踪代码提交

--- chapter10/config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
+++ chapter10/config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -3,11 +3,15 @@ package com.example.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
+@RefreshScope
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
@@ -16,6 +20,8 @@ public class ConfigClientApplication {
 
     @Value("${foo}")
     String foo;
+   /* @Value("${server.port}")
+    Integer port;*/
     @RequestMapping(value = "/foo")
     public String hi(){
         return foo;
